5760e7cfbad7efed05632f5e081d3574aa2a1a17
Rafael Reis <rafael.araujoreis@hotmail.com>
2021-04-20 20:48:14 -0300

    Configuration global styles

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+/**
+ *  utilizado para configurar o que ficara por volta de nossas paginas (customizado)
+ *  o _document é carregado apenas uma vez
+ * 
+ *  utizado /> para fechar a tag link devido ao padrão do React
+ *  */
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+
