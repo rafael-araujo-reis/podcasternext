828658b26092263173654759c44ce85311de068f
Rafael Reis <rafael.araujoreis@hotmail.com>
2021-04-21 13:20:19 -0300

    Definition datas page episode

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -3,13 +3,15 @@
  * neste caso foi utilizado slug
  */
 import { GetStaticPaths, GetStaticProps } from 'next';
-import { useRouter } from 'next/router'
+import Image from 'next/image';
 
 import { format, parseISO } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+
+import styles from './episode.module.scss'
 
 type Episode = {
   id: string,
@@ -28,9 +30,33 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
-
   return (
-    <h1>{episode.title}</h1>
+    <div className={styles.episode}>
+      <div className={styles.thumbnailContainer}>
+        <button type="button">
+          <img src="/arrow-left.svg" alt="Voltar" />
+        </button>
+        <Image
+          width={700}
+          height={160}
+          src={episode.thumbnail}
+          objectFit="cover"
+        />
+        <button type="button">
+          <img src="/play.svg" alt="Tocar episódio" />
+        </button>
+      </div>
+
+      <header>
+        <h1>{episode.title}</h1>
+        <span>{episode.members}</span>
+        <span>{episode.publishedAt}</span>
+        <span>{episode.durationAsString}</span>
+
+        <div className={styles.description}
+          dangerouslySetInnerHTML={{ __html: episode.description }} />
+      </header>
+    </div>
   )
 }
 
