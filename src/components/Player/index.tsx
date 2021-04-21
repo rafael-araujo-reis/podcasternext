fdb2ebc6eef567d6b006ef413e7c79a28caffb01
Rafael Reis <rafael.araujoreis@hotmail.com>
2021-04-21 07:48:43 -0300

    Change posicion buttons

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -21,23 +21,23 @@ export function Player() {
           </div>
           <span>00:00</span>
 
-          <div className={styles.buttons}>
-            <button type="button">
-              <img src="/shuffle.svg" alt="Embaralhar" />
-            </button>
-            <button type="button">
-              <img src="/play-previous.svg" alt="Tocar anterior" />
-            </button>
-            <button type="button" className={styles.playerButton}>
-              <img src="/play.svg" alt="Tocar" />
-            </button>
-            <button type="button">
-              <img src="/play-next.svg" alt="Tocar próxima" />
-            </button>
-            <button type="button">
-              <img src="/repeat.svg" alt="Repetir" />
-            </button>
-          </div>
+        </div>
+        <div className={styles.buttons}>
+          <button type="button">
+            <img src="/shuffle.svg" alt="Embaralhar" />
+          </button>
+          <button type="button">
+            <img src="/play-previous.svg" alt="Tocar anterior" />
+          </button>
+          <button type="button" className={styles.playerButton}>
+            <img src="/play.svg" alt="Tocar" />
+          </button>
+          <button type="button">
+            <img src="/play-next.svg" alt="Tocar próxima" />
+          </button>
+          <button type="button">
+            <img src="/repeat.svg" alt="Repetir" />
+          </button>
         </div>
       </footer>
     </div>
