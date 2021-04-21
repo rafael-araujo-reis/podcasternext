29c45cb45c6c60618c615a64040172255be285d9
Rafael Reis <rafael.araujoreis@hotmail.com>
2021-04-20 21:59:34 -0300

    Style component player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,7 +1,10 @@
+import styles from './styles.module.scss'
+
 export function Player() {
 
   return (
-    <>
-    </>
+    <div className={styles.playerContainer}>
+
+    </div>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
-import { Header } from '../components/Header';
 import '../styles/global.scss'
+
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+
+import styles from '../styles/app.module.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
   );
 }
 
