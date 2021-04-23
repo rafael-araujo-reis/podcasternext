68eb34dfc8b183b1c661ea1e36e1fb218d819355
Rafael Reis <rafael.araujoreis@hotmail.com>
2021-04-22 21:09:24 -0300

    Carregando  os últimos episódios de forma estática e explicação do incremental static regeneration

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -65,8 +65,53 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+
+  /**
+   * Quando passamos os paths vazios, nenhuma página estática é gerado no momento da build;
+   * Se o fallback for enviado como false, caso o usuário tente acessar uma pagina diferente do que existe no servidor,
+   * o retorno será 404
+   * 
+   * Caso seja passado valores nos paths e o fallback estiver como false, apenas os valores atribuídos no path estarão disponíveis
+   * O fallback blocking retorna as paginas 
+   * 
+   * Passando valores no paths e fallback true, caso o cliente tente acessar um episódio do não gerado no path, o fallback
+   * tentará gerar esse episódio, porém executado do lado do client
+   * Para não dar problema no carregamento das páginas, é necessário importar o hook do next (useRouter) e incluir uma 
+   * verificação if(userRouter.fallback){return 'Carregando...'}
+   * 
+   * Utilizando o fallback: 'blocking', o carregamento da página ocorre no next.js (node.js), assim o cliente será 
+   * direcionado apenas quando o conteúdo estiver carregado na página a ser acessada
+   * 
+   * 
+   * **************************************************
+   * Exemplo:
+   * Pensando em um serviço com 10 opções, poderia ser gerado as 10 opções diretos no path, para uma melhor experiência 
+   * do cliente, isso dificílmente iria onerar o build
+   * Pensando em um serviço com 10.000 opções, poderia ser gerado no path os 50 mais acessados, e os demais conforme as 
+   * requisições dos clientes. Se eu fosse gerar todas as 10.000 requisições, provavelmente iria afetar o build da aplicação 
+   * 
+   * No next esse processo chama-se incremental static regeneration (fallback true and blocking)
+   * **************************************************
+   */
+
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  })
+
   return {
-    paths: [],
+    paths,
     fallback: 'blocking'
   }
 }
